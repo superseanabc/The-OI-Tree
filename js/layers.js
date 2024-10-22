81e70eb5f3f4dcdc060da997e4cb3ae379e994d7
Tritium Coding <duyu1975@outlook.com>
2024-10-22 18:58:57 +0800

    Add stuff

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,15 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("L", {
+    name: "learn code", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "L", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#aaaaaa",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "code", // Name of prestige currency
+    baseResource: "code xp", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -17,12 +17,19 @@ addLayer("p", {
         mult = new Decimal(1)
         return mult
     },
+    upgrades: {
+        11: {
+            title:"Into programming",
+            description: "Increase code xp generation by 1.",
+            cost: new Decimal(10)
+        },
+    },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "l", description: "L: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "The OI Tree",
 	id: "OI",
 	author: "d909RCA",
-	pointsName: "points",
+	pointsName: "code xp",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,13 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Basic Algorithms",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
+	<h3>v0.1</h3><br>
+		- Created this in five minutes, laughed the whole afternoon.<br>
 		- Added stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
